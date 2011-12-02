1064ed023a25a9b924b78285a6ef571ae15f90e2
Luke Plant <L.Plant.98@cantab.net>
2011-12-02 00:19:22 +0000

    Added PyPI Python version trove classifiers

--- setup.py
+++ setup.py
@@ -32,6 +32,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Framework :: Django",
         "Topic :: Software Development :: Testing",
         "Topic :: Database"
